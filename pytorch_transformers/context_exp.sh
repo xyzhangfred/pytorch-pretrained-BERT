6bd7a2609c243de8a23ec5b6a244d2b5b59262f8
fredxyz <fredzhang922@gmail.com>
2019-08-08 16:46:40 -0400

    fuzzy_exp

---
+++ pytorch_transformers/context_exp.sh
@@ -0,0 +1,56 @@
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 2;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 5;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 10;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 20;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 50;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 100 ;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 200 ;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 500 ;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 1000;
+
+python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 400 ;
+
+python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 600 ;
+
+python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 800;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 1  --do_local_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 2  --do_local_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 5  --do_local_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 10  --do_local_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 20  --do_local_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 100  --do_local_shuffle >>local_shuffle.log;
+
+python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 200  --do_local_shuffle >>local_shuffle.log;
+
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 298  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 295  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 290  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 280  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 250  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 200  --do_global_shuffle >>local_shuffle.log;
+
+#python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 100  --do_global_shuffle >>local_shuffle.log;
+
+
+python train_gpt2.py --output_dir ./fine_tuned_model --do_eval --tokenized --load_finetuned --context_length 300 --shuffle_pos 200  --do_global_shuffle >>local_shuffle.log;
+
